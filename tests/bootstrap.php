c54f54750ca09fcb6f321eb4cd0dcfc111d155fa
Brandon Lamb <brandon@brandonlamb.com>
2014-10-09 10:04:24 -0700

    - Update composer - Update gitignore - Move lib to src

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,6 +1,9 @@
 <?php
+
 $classLoader = include(__DIR__ . '/../vendor/autoload.php');
 
-/* @var $classLoader \Composer\Autoload\ClassLoader */
+/**
+ * @var $classLoader \Composer\Autoload\ClassLoader 
+ */
 $classLoader->add('PAXB\\Tests\\', __DIR__ . '/');
 unset($classLoader);
